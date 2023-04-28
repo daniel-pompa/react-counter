e8a8bf528c8079b1a118618092fb58f38462859a
Daniel <daniel.40.pompa@gmail.com>
2023-04-28 22:15:12 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/images/react.svg';
+
+function App() {
+  return (
+    <div className='app'>
+      <div>
+        <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default App;
