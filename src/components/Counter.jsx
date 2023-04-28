2ada9f8e644cac7933893597025bc94a7d11c3b3
Daniel <daniel.40.pompa@gmail.com>
2023-04-28 23:14:13 +0200

    Increment, decrement, and reset counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,13 +4,28 @@ import PropTypes from 'prop-types';
 const Counter = ({ value }) => {
   const [counter, setCounter] = useState(value);
 
+  // Function to increment the counter by one
+  const handleAdd = () => setCounter(counter + 1);
+
+  // Function to decrement the counter by one
+  const handleSubtract = () => setCounter(counter - 1);
+
+  // Function to reset the counter to zero
+  const handleReset = () => setCounter(value);
+
   return (
     <>
       <h2 id='counter'>{counter}</h2>
       <div className='buttons'>
-        <button className='btn'>+</button>
-        <button className='btn btn-reset'>Reset</button>
-        <button className='btn'>-</button>
+        <button onClick={handleAdd} className='btn'>
+          +
+        </button>
+        <button onClick={handleReset} className='btn btn-reset'>
+          Reset
+        </button>
+        <button onClick={handleSubtract} className='btn'>
+          -
+        </button>
       </div>
     </>
   );
